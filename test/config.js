23f9f5e06b517b715d35ef4846548092cee4efe2
Evgeny Vlasenko <mahnunchik@gmail.com>
2016-01-08 22:44:56 +0700

    added new options from sharp, added tests for config

--- test/config.js
+++ test/config.js
@@ -5,110 +5,118 @@
 var assert = require('assert');
 var prepareConfig = require('../lib/config');
 
-describe('gulp-responsive', function() {
+describe('gulp-responsive', function () {
 
   describe('config', function () {
 
-    it('should set unspecified default value for `withoutEnlargement`', function() {
+    it('should set default values for unspecified config options', function () {
       var config = prepareConfig([{
         name: 'gulp.png',
-        width: 100
       }]);
 
       assert.equal(config.length, 1);
       assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].withoutEnlargement, true);
-    });
-
-    it('should set unspecified default value for `skipOnEnlargement`', function() {
-      var config = prepareConfig([{
-        name: 'gulp.png',
-        width: 100
-      }]);
-
-      assert.equal(config.length, 1);
-      assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].skipOnEnlargement, false);
-    });
-
-    it('should set unspecified default value for `quality`', function() {
-      var config = prepareConfig([{
-        name: 'gulp.png',
-        width: 100
-      }]);
-
-      assert.equal(config.length, 1);
-      assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].quality, 80);
-    });
-
-    it('should set unspecified default value for `progressive`', function() {
-      var config = prepareConfig([{
-        name: 'gulp.png',
-        width: 100
-      }]);
-
-      assert.equal(config.length, 1);
-      assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].progressive, false);
-    });
-
-    it('should set unspecified default value for `compressionLevel`', function() {
-      var config = prepareConfig([{
-        name: 'gulp.png',
-        width: 100
-      }]);
-
-      assert.equal(config.length, 1);
-      assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].compressionLevel, 6);
-    });
-
-    it('should set unspecified default value for `withMetadata`', function() {
-      var config = prepareConfig([{
-        name: 'gulp.png',
-        width: 100
-      }]);
-
-      assert.equal(config.length, 1);
-      assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].withMetadata, false);
-    });
-
-    it('should set unspecified default value for `max`', function() {
-      var config = prepareConfig([{
-        name: 'gulp.png',
-        width: 100
-      }]);
-
-      assert.equal(config.length, 1);
-      assert.equal(config[0].name, 'gulp.png');
+      assert.equal(config[0].crop, false);
+      assert.equal(config[0].embed, false);
+      assert.equal(config[0].min, false);
       assert.equal(config[0].max, false);
+      assert.equal(config[0].withoutEnlargement, true);
+      assert.equal(config[0].skipOnEnlargement, false);
+      assert.equal(config[0].ignoreAspectRatio, false);
+      assert.equal(config[0].interpolation, 'bicubic');
+      assert.equal(config[0].extractBeforeResize, false);
+      assert.equal(config[0].extractAfterResize, false);
+      assert.equal(config[0].background, '#fff');
+      assert.equal(config[0].flatten, false);
+      assert.equal(config[0].negate, false);
+      assert.equal(config[0].rotate, false);
+      assert.equal(config[0].flip, false);
+      assert.equal(config[0].flop, false);
+      assert.equal(config[0].blur, false);
+      assert.equal(config[0].sharpen, false);
+      assert.equal(config[0].threshold, false);
+      assert.equal(config[0].gamma, false);
+      assert.equal(config[0].grayscale, false);
+      assert.equal(config[0].normalize, false);
+      assert.equal(config[0].quality, 80);
+      assert.equal(config[0].progressive, false);
+      assert.equal(config[0].withMetadata, false);
+      assert.equal(config[0].tile, false);
+      assert.equal(config[0].withoutChromaSubsampling, false);
+      assert.equal(config[0].compressionLevel, 6);
+      assert.equal(config[0].format, null);
     });
-    
-    it('should not override specified values', function() {
+
+    it('should not override specified values', function () {
       var config = prepareConfig([{
         name: 'gulp.png',
         width: 100,
-        withoutEnlargement: false,
-        skipOnEnlargement: true,
-        quality: 96,
-        progressive: true,
-        compressionLevel: 8,
-        withMetadata: true,
-        max: true
+        crop: 'test',
+        embed: 'test',
+        min: 'test',
+        max: 'test',
+        withoutEnlargement: 'test',
+        skipOnEnlargement: 'test',
+        ignoreAspectRatio: 'test',
+        interpolation: 'test',
+        extractBeforeResize: 'test',
+        extractAfterResize: 'test',
+        background: 'test',
+        flatten: 'test',
+        negate: 'test',
+        rotate: 'test',
+        flip: 'test',
+        flop: 'test',
+        blur: 'test',
+        sharpen: 'test',
+        threshold: 'test',
+        gamma: 'test',
+        grayscale: 'test',
+        normalize: 'test',
+        quality: 'test',
+        progressive: 'test',
+        withMetadata: 'test',
+        tile: 'test',
+        withoutChromaSubsampling: 'test',
+        compressionLevel: 'test',
+        format: 'test',
       }]);
 
       assert.equal(config.length, 1);
       assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].withoutEnlargement, false);
-      assert.equal(config[0].skipOnEnlargement, true);
-      assert.equal(config[0].quality, 96);
-      assert.equal(config[0].progressive, true);
-      assert.equal(config[0].compressionLevel, 8);
-      assert.equal(config[0].withMetadata, true);
-      assert.equal(config[0].max, true);
+      assert.equal(config[0].crop, 'test');
+      assert.equal(config[0].embed, 'test');
+      assert.equal(config[0].min, 'test');
+      assert.equal(config[0].max, 'test');
+      assert.equal(config[0].withoutEnlargement, 'test');
+      assert.equal(config[0].skipOnEnlargement, 'test');
+      assert.equal(config[0].ignoreAspectRatio, 'test');
+      assert.equal(config[0].interpolation, 'test');
+      assert.equal(config[0].extractBeforeResize, 'test');
+      assert.equal(config[0].extractAfterResize, 'test');
+      assert.equal(config[0].background, 'test');
+      assert.equal(config[0].flatten, 'test');
+      assert.equal(config[0].negate, 'test');
+      assert.equal(config[0].rotate, 'test');
+      assert.equal(config[0].flip, 'test');
+      assert.equal(config[0].flop, 'test');
+      assert.equal(config[0].blur, 'test');
+      assert.equal(config[0].sharpen, 'test');
+      assert.equal(config[0].threshold, 'test');
+      assert.equal(config[0].gamma, 'test');
+      assert.equal(config[0].grayscale, 'test');
+      assert.equal(config[0].normalize, 'test');
+      assert.equal(config[0].quality, 'test');
+      assert.equal(config[0].progressive, 'test');
+      assert.equal(config[0].withMetadata, 'test');
+      assert.equal(config[0].tile, 'test');
+      assert.equal(config[0].withoutChromaSubsampling, 'test');
+      assert.equal(config[0].compressionLevel, 'test');
+      assert.equal(config[0].format, 'test');
     });
+  });
+
+  describe('config parsing', function () {
 
     it('should parse config object', function() {
       var config = prepareConfig({
@@ -119,8 +127,6 @@ describe('gulp-responsive', function() {
 
       assert.equal(config.length, 1);
       assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].withoutEnlargement, true);
-      assert.equal(config[0].skipOnEnlargement, false);
     });
 
     it('should parse config object of arrays', function() {
@@ -134,26 +140,63 @@ describe('gulp-responsive', function() {
 
       assert.equal(config.length, 2);
       assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].withoutEnlargement, true);
-      assert.equal(config[0].skipOnEnlargement, false);
+      assert.equal(config[0].width, 100);
       assert.equal(config[1].name, 'gulp.png');
-      assert.equal(config[1].withoutEnlargement, true);
-      assert.equal(config[1].skipOnEnlargement, false);
+      assert.equal(config[1].width, 200);
     });
 
+    it('should leave config array as is', function() {
+      var config = prepareConfig(
+        [{
+          name: 'gulp1.png',
+          width: 100,
+        },{
+          name: 'gulp2.png',
+          width: 200,
+        }]
+      );
+
+      assert.equal(config.length, 2);
+      assert.equal(config[0].name, 'gulp1.png');
+      assert.equal(config[0].width, 100);
+      assert.equal(config[1].name, 'gulp2.png');
+      assert.equal(config[1].width, 200);
+    });
   });
 
   describe('global config', function () {
 
     it('should override default values', function () {
       var globalConfig = {
-        withoutEnlargement: false,
-        skipOnEnlargement: true,
-        quality: 50,
-        progressive: true,
-        compressionLevel: 5,
-        withMetadata: true,
-        max: true
+        crop: 'test',
+        embed: 'test',
+        min: 'test',
+        max: 'test',
+        withoutEnlargement: 'test',
+        skipOnEnlargement: 'test',
+        ignoreAspectRatio: 'test',
+        interpolation: 'test',
+        extractBeforeResize: 'test',
+        extractAfterResize: 'test',
+        background: 'test',
+        flatten: 'test',
+        negate: 'test',
+        rotate: 'test',
+        flip: 'test',
+        flop: 'test',
+        blur: 'test',
+        sharpen: 'test',
+        threshold: 'test',
+        gamma: 'test',
+        grayscale: 'test',
+        normalize: 'test',
+        quality: 'test',
+        progressive: 'test',
+        withMetadata: 'test',
+        tile: 'test',
+        withoutChromaSubsampling: 'test',
+        compressionLevel: 'test',
+        format: 'test',
       };
       var config = prepareConfig([{
         name: 'gulp.png'
@@ -161,46 +204,134 @@ describe('gulp-responsive', function() {
 
       assert.equal(config.length, 1);
       assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].withoutEnlargement, false);
-      assert.equal(config[0].skipOnEnlargement, true);
-      assert.equal(config[0].quality, 50);
-      assert.equal(config[0].progressive, true);
-      assert.equal(config[0].compressionLevel, 5);
-      assert.equal(config[0].withMetadata, true);
-      assert.equal(config[0].max, true);
+      assert.equal(config[0].crop, 'test');
+      assert.equal(config[0].embed, 'test');
+      assert.equal(config[0].min, 'test');
+      assert.equal(config[0].max, 'test');
+      assert.equal(config[0].withoutEnlargement, 'test');
+      assert.equal(config[0].skipOnEnlargement, 'test');
+      assert.equal(config[0].ignoreAspectRatio, 'test');
+      assert.equal(config[0].interpolation, 'test');
+      assert.equal(config[0].extractBeforeResize, 'test');
+      assert.equal(config[0].extractAfterResize, 'test');
+      assert.equal(config[0].background, 'test');
+      assert.equal(config[0].flatten, 'test');
+      assert.equal(config[0].negate, 'test');
+      assert.equal(config[0].rotate, 'test');
+      assert.equal(config[0].flip, 'test');
+      assert.equal(config[0].flop, 'test');
+      assert.equal(config[0].blur, 'test');
+      assert.equal(config[0].sharpen, 'test');
+      assert.equal(config[0].threshold, 'test');
+      assert.equal(config[0].gamma, 'test');
+      assert.equal(config[0].grayscale, 'test');
+      assert.equal(config[0].normalize, 'test');
+      assert.equal(config[0].quality, 'test');
+      assert.equal(config[0].progressive, 'test');
+      assert.equal(config[0].withMetadata, 'test');
+      assert.equal(config[0].tile, 'test');
+      assert.equal(config[0].withoutChromaSubsampling, 'test');
+      assert.equal(config[0].compressionLevel, 'test');
+      assert.equal(config[0].format, 'test');
     });
 
     it('should not override values specified per file', function () {
       var globalConfig = {
-        withoutEnlargement: true,
-        skipOnEnlargement: false,
-        quality: 50,
-        progressive: false,
-        compressionLevel: 5,
-        withMetadata: false,
-        max: true
+        crop: 'global',
+        embed: 'global',
+        min: 'global',
+        max: 'global',
+        withoutEnlargement: 'global',
+        skipOnEnlargement: 'global',
+        ignoreAspectRatio: 'global',
+        interpolation: 'global',
+        extractBeforeResize: 'global',
+        extractAfterResize: 'global',
+        background: 'global',
+        flatten: 'global',
+        negate: 'global',
+        rotate: 'global',
+        flip: 'global',
+        flop: 'global',
+        blur: 'global',
+        sharpen: 'global',
+        threshold: 'global',
+        gamma: 'global',
+        grayscale: 'global',
+        normalize: 'global',
+        quality: 'global',
+        progressive: 'global',
+        withMetadata: 'global',
+        tile: 'global',
+        withoutChromaSubsampling: 'global',
+        compressionLevel: 'global',
+        format: 'global',
       };
       var config = prepareConfig([{
         name: 'gulp.png',
         width: 100,
-        withoutEnlargement: false,
-        skipOnEnlargement: true,
-        quality: 96,
-        progressive: true,
-        compressionLevel: 8,
-        withMetadata: true,
-        max: false
+        crop: 'test',
+        embed: 'test',
+        min: 'test',
+        max: 'test',
+        withoutEnlargement: 'test',
+        skipOnEnlargement: 'test',
+        ignoreAspectRatio: 'test',
+        interpolation: 'test',
+        extractBeforeResize: 'test',
+        extractAfterResize: 'test',
+        background: 'test',
+        flatten: 'test',
+        negate: 'test',
+        rotate: 'test',
+        flip: 'test',
+        flop: 'test',
+        blur: 'test',
+        sharpen: 'test',
+        threshold: 'test',
+        gamma: 'test',
+        grayscale: 'test',
+        normalize: 'test',
+        quality: 'test',
+        progressive: 'test',
+        withMetadata: 'test',
+        tile: 'test',
+        withoutChromaSubsampling: 'test',
+        compressionLevel: 'test',
+        format: 'test',
       }], globalConfig);
 
       assert.equal(config.length, 1);
       assert.equal(config[0].name, 'gulp.png');
-      assert.equal(config[0].withoutEnlargement, false);
-      assert.equal(config[0].skipOnEnlargement, true);
-      assert.equal(config[0].quality, 96);
-      assert.equal(config[0].progressive, true);
-      assert.equal(config[0].compressionLevel, 8);
-      assert.equal(config[0].withMetadata, true);
-      assert.equal(config[0].max, false);
+      assert.equal(config[0].crop, 'test');
+      assert.equal(config[0].embed, 'test');
+      assert.equal(config[0].min, 'test');
+      assert.equal(config[0].max, 'test');
+      assert.equal(config[0].withoutEnlargement, 'test');
+      assert.equal(config[0].skipOnEnlargement, 'test');
+      assert.equal(config[0].ignoreAspectRatio, 'test');
+      assert.equal(config[0].interpolation, 'test');
+      assert.equal(config[0].extractBeforeResize, 'test');
+      assert.equal(config[0].extractAfterResize, 'test');
+      assert.equal(config[0].background, 'test');
+      assert.equal(config[0].flatten, 'test');
+      assert.equal(config[0].negate, 'test');
+      assert.equal(config[0].rotate, 'test');
+      assert.equal(config[0].flip, 'test');
+      assert.equal(config[0].flop, 'test');
+      assert.equal(config[0].blur, 'test');
+      assert.equal(config[0].sharpen, 'test');
+      assert.equal(config[0].threshold, 'test');
+      assert.equal(config[0].gamma, 'test');
+      assert.equal(config[0].grayscale, 'test');
+      assert.equal(config[0].normalize, 'test');
+      assert.equal(config[0].quality, 'test');
+      assert.equal(config[0].progressive, 'test');
+      assert.equal(config[0].withMetadata, 'test');
+      assert.equal(config[0].tile, 'test');
+      assert.equal(config[0].withoutChromaSubsampling, 'test');
+      assert.equal(config[0].compressionLevel, 'test');
+      assert.equal(config[0].format, 'test');
     });
   });
 });
